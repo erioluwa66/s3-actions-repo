ad97ec78a89a9ca4b1362cc0ae24c383c5791291
erioluwa66 <erioluwa66@gmail.com>
2023-09-24 06:09:15 -0400

    backend

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 module "s3" {
-  source = "git@github.com:erioluwa66/s3-backend-repo.git//s3module?ref=v1.1.1"
+  source = "git@github.com:erioluwa66/s3-backend-repo.git//s3module?ref=v1.2.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "Landmark-automation-mariam"
+    region  = "us-east-2"
+    key     = "action/terraform.tfstate"
+    encrypt = true
+
+  }
+
+
 }
