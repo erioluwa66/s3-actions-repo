55e0064bc5eaefc7969606fceac25fff414965cc
erioluwa66 <erioluwa66@gmail.com>
2023-09-24 06:43:50 -0400

    module update

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "s3" {
   source = "git@github.com:erioluwa66/s3-backend-repo.git//s3module?ref=v1.2.1"
 }
 
-#backend configuration
+#backend configurations
 terraform {
   required_providers {
     aws = {
